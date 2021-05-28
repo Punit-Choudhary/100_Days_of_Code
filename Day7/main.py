31c6219c7a38cd2e143362d67620b7e05bc10be4
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-05-28 17:30:55 +0530

    Renamed file

---
+++ Day7/main.py
@@ -0,0 +1,96 @@
+# Hangman
+
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ['apple', 'ball', 'camel']
+choosen_word = random.choice(word_list)
+
+print(f"Choosen word is {choosen_word}.")
+
+lives = 6
+
+display = []
+for _ in range(len(choosen_word)):
+    display.append("_")
+
+game_over = False
+
+while not game_over:
+    print(stages[lives])
+    guess = input("Guess a letter: ").lower()
+
+    for index in range(len(choosen_word)):
+        letter = choosen_word[index]
+    
+        if letter == guess:
+            display[index] = guess
+    
+    print(''.join(display))
+    
+    if guess not in choosen_word:
+      lives -= 1
+      print("You guessed it wrong")
+      if lives == 0:
+        print(f"You loose, it was {choosen_word}!")
+        game_over = True
+
+    if "_" not in display:
+        print("You win!")
+        game_over = True
