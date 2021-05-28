f9a684d5752be14d162ef66684e070bcb857049f
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-05-28 17:59:43 +0530

    Day7 Project complete

--- Day7/main.py
+++ Day7/main.py
@@ -7,12 +7,15 @@ main.py -- contains game logic.
 """
 
 import random
+import words
+import art
 
-
-word_list = ['apple', 'ball', 'camel']
+word_list = words.word_list
+stages = art.stages
+logo = art.logo
 choosen_word = random.choice(word_list)
 
-print(f"Choosen word is {choosen_word}.")
+print(logo)
 
 lives = 6
 
@@ -23,24 +26,28 @@ for _ in range(len(choosen_word)):
 game_over = False
 
 while not game_over:
-    print(stages[lives])
     guess = input("Guess a letter: ").lower()
-
-    for index in range(len(choosen_word)):
+    
+    if guess not in display:
+      for index in range(len(choosen_word)):
         letter = choosen_word[index]
-    
+        
         if letter == guess:
-            display[index] = guess
-    
-    print(''.join(display))
+          display[index] = guess
+    else:
+      print(f"You have already guessed {guess}")
     
     if guess not in choosen_word:
       lives -= 1
-      print("You guessed it wrong")
+      print(f"You guessed {guess}, that's not in the word. You lose a life.")
       if lives == 0:
         print(f"You loose, it was {choosen_word}!")
         game_over = True
+    
+    print(' '.join(display))
 
     if "_" not in display:
         print("You win!")
         game_over = True
+    
+    print(stages[lives])
