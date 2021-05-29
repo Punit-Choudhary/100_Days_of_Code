ed4e6bde53901faab5e493939e8fccb913a0ea4d
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-05-29 20:22:47 +0530

    Day8 project-step1

---
+++ Day8/caesar_cipher.py
@@ -0,0 +1,18 @@
+# Caesar Cipher -- Encoder, Decoder
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text, shift):
+    encoded_text = ""
+    for letter in text:
+        position = alphabet.index(letter) + shift
+        encoded_text += alphabet[position]
+    print(f"The encoded text is {encoded_text}")
+
+if direction == "encode":
+    encrypt(text, shift)
