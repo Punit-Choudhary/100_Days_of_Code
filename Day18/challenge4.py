f21ad883c3fc2969f53da5f973599a70776d81df
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-07-15 23:13:46 +0530

    Done challenge 4 part 2

--- Day18/challenge4.py
+++ Day18/challenge4.py
@@ -1,17 +1,22 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 import random
 
 kachua = Turtle()
 s = Screen()
+colormode(255)
 
-colors = ["blue", "red", "purple", "green", "cyan", "violet", "brown"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
 
 kachua.pensize(5)
 kachua.speed(9)
 kachua.forward(10)
 angles = [90, 180, 270, 90]
 for _ in range(300):
-    kachua.pencolor(random.choice(colors))
+    kachua.pencolor(random_color())
     kachua.right(random.choice(angles))
     kachua.forward(random.randint(25, 75))
 
